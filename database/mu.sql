f0589aa4a322e422a25791dc98efe1dee6c220b6
Seb Insua <IM.Hanz@gmail.com>
2011-09-03 14:12:40 +0100

    Database has been fixed to a format more suitable for a Python project...

--- database/mu.sql
+++ database/mu.sql
@@ -1,105 +1,99 @@
 -- mu
 -- database v0.01
 
-\connect root
-
 SET client_encoding = 'UTF8';
-
-CREATE DATABASE mu;
-
-\connect mu root
 
 BEGIN;
 
 CREATE TABLE Users (
-	userID		serial,
-	UUID		varchar(100) NOT NULL,
-	email		text NOT NULL,
-	userName	text NOT NULL,
-	passwordHash	char(60) NOT NULL,
-	firstName	text,
-	lastName	text,
-	gender		char(1),
-	birthdate	date,
-	summary		text,
-	created		timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (userID),
-	UNIQUE (UUID, email, userName)
+	user_id			serial,
+	uuid			varchar(100) NOT NULL,
+	email			text NOT NULL,
+	username		text NOT NULL,
+	password_hash	char(60) NOT NULL,
+	first_name		text,
+	last_name		text,
+	gender			char(1),
+	date_of_birth	date,
+	summary			text,
+	created			timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (user_id),
+	UNIQUE (uuid, email, username)
 );
 
 CREATE TABLE ContentAuthors (
-	contentAuthorID	serial,
-	musicBrainzMBID	char(36),
-	name		text NOT NULL,
-	sortName	varchar(50),
-	startDate	timestamp,
-	endDate		timestamp,
-	created		timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (contentAuthorID)
+	content_author_id	serial,
+	musicbrainz_mbid	char(36),
+	name				text NOT NULL,
+	sort_name			varchar(50),
+	start_date			timestamp,
+	end_date			timestamp,
+	created				timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (content_author_id)
 );
 
 CREATE TABLE UserContentAuthors (
-	userContentAuthorID	serial,
-	userID			integer NOT NULL,
-	contentAuthorID		integer NOT NULL,
-	created			timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (userContentAuthorID),
-	FOREIGN KEY (userID) REFERENCES Users,
-	FOREIGN KEY (contentAuthorID) REFERENCES ContentAuthors
+	usercontent_author_id	serial,
+	user_id					integer NOT NULL,
+	content_author_id		integer NOT NULL,
+	created					timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (usercontent_author_id),
+	FOREIGN KEY (user_id) REFERENCES Users,
+	FOREIGN KEY (content_author_id) REFERENCES ContentAuthors
 );
 
 CREATE TABLE ReleaseMediums (
-	releaseMediumID	serial,
-	name		varchar(50) NOT NULL,
-	PRIMARY KEY (releaseMediumID),
+	release_medium_id	serial,
+	name				varchar(50) NOT NULL,
+	PRIMARY KEY (release_medium_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE ReleaseTypes (
-	releaseTypeID	serial,
-	name		varchar(50) NOT NULL,
-	PRIMARY KEY (releaseTypeID),
+	release_type_id	serial,
+	name			varchar(50) NOT NULL,
+	PRIMARY KEY (release_type_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE ReleaseStatuses (
-	releaseStatusID	serial,
-	name		varchar(50) NOT NULL,
-	PRIMARY KEY (releaseStatusID),
+	release_status_id	serial,
+	name				varchar(50) NOT NULL,
+	PRIMARY KEY (release_status_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE Releases (
-	releaseID	serial,
-	releaseTypeID	integer NOT NULL,
-	releaseStatusID	integer NOT NULL,
-	releaseMediumID	integer NOT NULL,
-	title		text NOT NULL,
-	sortTitle	varchar(50),
-	created		timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (releaseID),
-	FOREIGN KEY (releaseTypeID) REFERENCES ReleaseTypes,
-	FOREIGN KEY (releaseStatusID) REFERENCES ReleaseStatuses,
-	FOREIGN KEY (releaseMediumID) REFERENCES ReleaseMediums
+	release_id			serial,
+	release_type_id		integer NOT NULL,
+	release_status_id	integer NOT NULL,
+	release_medium_id	integer NOT NULL,
+	title				text NOT NULL,
+	sort_title			varchar(50),
+	created				timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (release_id),
+	FOREIGN KEY (release_type_id) REFERENCES ReleaseTypes,
+	FOREIGN KEY (release_status_id) REFERENCES ReleaseStatuses,
+	FOREIGN KEY (release_medium_id) REFERENCES ReleaseMediums
 );
 
 CREATE TABLE ContentAuthorReleaseTypes (
-	contentAuthorReleaseTypeID	serial,
-	name				varchar(50) NOT NULL,
-	PRIMARY KEY (contentAuthorReleaseTypeID),
+	content_author_release_type_id	serial,
+	name							varchar(50) NOT NULL,
+	PRIMARY KEY (content_author_release_type_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE ContentAuthorReleases (
-	contentAuthorReleaseID		serial,
-	contentAuthorID			integer NOT NULL,
-	releaseID			integer NOT NULL,
-	contentAuthorReleaseTypeID	integer NOT NULL,
-	created				timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (contentAuthorReleaseID),
-	FOREIGN KEY (contentAuthorID) REFERENCES ContentAuthors,
-	FOREIGN KEY (releaseID)	REFERENCES Releases,
-	FOREIGN KEY (contentAuthorReleaseTypeID) REFERENCES ContentAuthorReleaseTypes
+	content_author_release_id		serial,
+	content_author_id				integer NOT NULL,
+	release_id						integer NOT NULL,
+	content_author_release_type_id	integer NOT NULL,
+	created							timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (content_author_release_id),
+	FOREIGN KEY (content_author_id) REFERENCES ContentAuthors,
+	FOREIGN KEY (release_id)	REFERENCES Releases,
+	FOREIGN KEY (content_author_release_type_id) REFERENCES ContentAuthorReleaseTypes
 );
 
 CREATE TABLE EventTypes (
@@ -110,67 +104,71 @@ CREATE TABLE EventTypes (
 );
 
 CREATE TABLE Events (
-	eventID		serial,
+	event_id	serial,
 	eventTypeID	integer NOT NULL,
 	created		timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (eventID),
+	PRIMARY KEY (event_id),
 	FOREIGN KEY (eventTypeID) REFERENCES EventTypes
 );
 
 CREATE TABLE ReleaseEvents (
-	releaseEventID			serial,
-	releaseID			integer NOT NULL,
-	eventID				integer NOT NULL,
-	predictedReleaseDate		date,
-	predictedTextualReleaseDate	text,
-	created				timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (releaseEventID),
-	FOREIGN KEY (releaseID) REFERENCES Releases,
-	FOREIGN KEY (eventID) REFERENCES Events
+	releaseevent_id					serial,
+	release_id						integer NOT NULL,
+	event_id						integer NOT NULL,
+	predicted_release_date			date,
+	predicted_textual_release_date	text,
+	created							timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (releaseevent_id),
+	FOREIGN KEY (release_id) REFERENCES Releases,
+	FOREIGN KEY (event_id) REFERENCES Events
 );
 
 CREATE TABLE UserEventStatuses (
-	userEventStatusID	serial,
-	name			varchar(50) NOT NULL,
-	PRIMARY KEY (userEventStatusID),
+	user_event_status_id	serial,
+	name					varchar(50) NOT NULL,
+	PRIMARY KEY (user_event_status_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE UserEvents (
-	userEventID		serial,
-	userID			integer NOT NULL,
-	eventID			integer NOT NULL,
-	userEventStatusID	integer NOT NULL,
-	created			timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (userEventID),
-	FOREIGN KEY (userID) REFERENCES Users,
-	FOREIGN KEY (eventID) REFERENCES Events,
-	FOREIGN KEY (userEventStatusID) REFERENCES UserEventStatuses
+	user_event_id			serial,
+	user_id					integer NOT NULL,
+	event_id				integer NOT NULL,
+	user_event_status_id	integer NOT NULL,
+	created					timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (user_event_id),
+	FOREIGN KEY (user_id) REFERENCES Users,
+	FOREIGN KEY (event_id) REFERENCES Events,
+	FOREIGN KEY (user_event_status_id) REFERENCES UserEventStatuses
 );
 
 CREATE TABLE ContentOwners (
-	contentOwnerID	serial,
-	name		text NOT NULL,
-	sortName	varchar(50),
-	startDate	date,
-	endDate		date,
-	created		timestamp NOT NULL DEFAULT current_timestamp
+	content_owner_id	serial,
+	name				text NOT NULL,
+	sort_name			varchar(50),
+	start_date			date,
+	end_date			date,
+	created				timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (content_owner_id)
 );
 
 CREATE TABLE ContentOwnerReleaseTypes (
-	contentOwnerReleaseTypeID	serial,
-	name				varchar(50),
-	PRIMARY KEY (contentOwnerReleaseTypeID),
+	content_owner_release_type_id	serial,
+	name							varchar(50),
+	PRIMARY KEY (content_owner_release_type_id),
 	UNIQUE (name)
 );
 
 CREATE TABLE ContentOwnerReleases (
-	contentOwnerReleaseID		serial,
-	contentOwnerID			integer NOT NULL,
-	releaseID			integer NOT NULL,
-	contentOwnerReleaseTypeID	integer NOT NULL,
-	created				timestamp NOT NULL DEFAULT current_timestamp,
-	PRIMARY KEY (contentOwnerReleaseID)
+	content_owner_release_id		serial,
+	content_owner_id				integer NOT NULL,
+	release_id						integer NOT NULL,
+	content_owner_release_type_id	integer NOT NULL,
+	created							timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (content_owner_release_id),
+	FOREIGN KEY (content_owner_id) REFERENCES ContentOwners,
+	FOREIGN KEY (release_id) REFERENCES Releases,
+	FOREIGN KEY (content_owner_release_type_id) REFERENCES ContentOwnerReleaseTypes
 );
 
 COMMIT;
