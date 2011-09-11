6ce5d19699ceab8551afae21fdd26933971735b6
Seb Insua <IM.Hanz@gmail.com>
2011-09-11 23:02:11 +0100

    Support for flaskext.wtf has been added as well as the registration form. The code for actual registration and login has also been started, however this has not been finished: it needs code to be added to the UserDomain class, exception handling, use of the User entity model, etc... :)

--- modules/mu/controller/user.py
+++ modules/mu/controller/user.py
@@ -1,14 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, flash, render_template
 bp = Blueprint('user', __name__)
 
 @bp.route('/user/<username>')
 def show_user(username):
     pass
 
-@bp.route('/register')
+@bp.route('/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == "POST":
+        from mu.model.domain.user import UserDomain
+        user_domain = UserDomain()
+
+        if user_domain.login(request.form):
+            # It would actually be better to redirect to the original page
+            # we were on by passing this via request.form
+            redirect("/")
+
+    return render_template('login.html')
+
+@bp.route('/register', methods=['GET', 'POST'])
 def register_user():
-    from mu.model.user import User, db
-    # user = User('im.hanz@gmail.com', 'seb', 'password')
-    # db.session.add(user)
-    # db.session.commit()
-    return 'nothing here'
+    from mu.form.registration import RegistrationForm
+    registration_form = RegistrationForm(request.form)
+
+    if request.method == "POST" and registration_form.validate():
+        # We only need to access the UserDomain when we are passed
+        # POST data with which to attempt user registration.
+        from mu.model.domain.user import UserDomain
+        user_domain = UserDomain()
+
+        try:
+            user_id = user_domain.register(request.form)
+            user_domain.force_login(user_id)
+            flash("Thanks for registering!")
+            return redirect("/")
+        except:
+            # Capture particular exception messages
+            # and flash these on the register page.
+            flash("There has been a problem with your registration...")
+
+    return render_template('register.html', form=registration_form)
