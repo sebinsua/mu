32004e57f92017997d0eccc8191c3466328a5bde
Seb Insua <IM.Hanz@gmail.com>
2011-09-17 15:03:47 +0100

    Entities are closer to completion, but I've still got fuckloads of cookie cutter bullshit to write. fgs.

--- modules/app/model/entity/user.py
+++ modules/app/model/entity/user.py
@@ -19,11 +19,45 @@ class User(db.Model):
     summary = db.Column(db.Text)
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, username, email, password):
+    def __init__(self, username, email, password, first_name=None, \
+            last_name=None, gender=None, date_of_birth=None, summary=None):
         self.uuid = str(uuid.uuid1())
         self.username = username
         self.email = email
         self.password_hash = generate_password_hash(password)
 
+    def __repr__(self):
+        print '<User %r>' % self.username
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class UserContentAuthor(db.Model):
+    user_content_author_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    content_author_id = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, user_id, content_author_id):
+        self.user_id = user_id
+        self.content_author_id = content_author_id
+
+class UserEventStatus(db.Model):
+    user_event_status_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, user_event_status_id, name):
+        self.user_event_status_id = user_event_status_id
+        self.name = name
+
+class UserEvent(db.Model):
+    user_event_id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, nullable=False)
+    event_id= db.Column(db.Integer, nullable=False)
+    user_event_status_id = db.Column(db.Integer)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, user_id, event_id, user_event_status_id=None):
+        self.user_id = user_id
+        self.event_id = event_id
+        self.user_event_status_id = user_event_status_id
