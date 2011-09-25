66a9dae60cc2ffc26a77804f479997d38243c552
Seb Insua <IM.Hanz@gmail.com>
2011-09-25 01:50:30 +0100

    Switched to using proper quoted tablenames, which are therefore case-sensitive.

--- architecture/database/defaults.sql
+++ architecture/database/defaults.sql
@@ -3,7 +3,7 @@
 
 -- default ContentAuthorProductTypes
 -- NOTE: It is possible for a content author to be a songwriter for one product, and the lead singer for another.
-INSERT INTO ContentAuthorProductTypes (name) VALUES ('Artist');
+INSERT INTO "ContentAuthorProductTypes" (name) VALUES ('Artist');
 
 -- default ContentAuthorTypes
 -- TODO: There needs to be some way of (1) storing different people against one artist, or (2) featuring people against
@@ -11,21 +11,21 @@ INSERT INTO ContentAuthorProductTypes (name) VALUES ('Artist');
 -- NOTE: (1) can be eventually solved by adding multiple ContentAuthorContentAuthorTypes against a new ContentAuthorContentAuthors
 --           table which would store the ephemeral nature, also. The types would be songwriter, guitarist, bassist, etc...
 -- NOTE: (2) can be eventually solved by adding multiple ContentAuthorProductTypes, like: Remixer, Producer, Artist. See above.
--- INSERT INTO ContentAuthorContentAuthorTypes (name) VALUES ('Person'), ('Group'), ('Other'), ('Unknown');
+-- INSERT INTO "ContentAuthorContentAuthorTypes" (name) VALUES ('Person'), ('Group'), ('Other'), ('Unknown');
 
 -- default ContentOwnerProductTypes
 -- NOTE: It is possible for a content owner to be a distributor for one product, but something else for another.
-INSERT INTO ContentOwnerProductTypes (name) VALUES ('Original Production'), ('Bootleg Production'), ('Reissue Production'), ('Distributor'), ('Holding'), ('Unknown');
+INSERT INTO "ContentOwnerProductTypes" (name) VALUES ('Original Production'), ('Bootleg Production'), ('Reissue Production'), ('Distributor'), ('Holding'), ('Unknown');
 
 -- default EventTypes
 -- NOTE: I don't think we need to do more than an implicit list of (type: Event) products against a work to symbolise a festival.
-INSERT INTO EventTypes (name) VALUES ('Release'), ('Performance');
+INSERT INTO "EventTypes" (name) VALUES ('Release'), ('Performance');
 
 -- default ProductTypes
-INSERT INTO ProductTypes (name) VALUES ('Event'), ('Album'), ('EP'), ('Single');
+INSERT INTO "ProductTypes" (name) VALUES ('Event'), ('Album'), ('EP'), ('Single');
 
 -- default ProductStatuses
-INSERT INTO ProductStatuses (name) VALUES ('Official'), ('Promotional'), ('Bootleg');
+INSERT INTO "ProductStatuses" (name) VALUES ('Official'), ('Promotional'), ('Bootleg');
 
 -- default ProductMediums
-INSERT INTO ProductMediums (name) VALUES ('Live'), ('Digital'), ('CD'), ('DVD'), ('Vinyl'), ('Cassette');
+INSERT INTO "ProductMediums" (name) VALUES ('Live'), ('Digital'), ('CD'), ('DVD'), ('Vinyl'), ('Cassette');
--- architecture/database/latest.sql
+++ architecture/database/latest.sql
@@ -3,7 +3,7 @@
 
 BEGIN;
 
-CREATE TABLE Users (
+CREATE TABLE "Users" (
 	user_id			serial,
 	uuid			varchar(100) NOT NULL,
 	email			text NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE Users (
   UNIQUE (username)
 );
 
-CREATE TABLE ContentAuthors (
+CREATE TABLE "ContentAuthors" (
 	content_author_id	serial,
 	musicbrainz_mbid	char(36),
 	name				text NOT NULL,
@@ -32,55 +32,55 @@ CREATE TABLE ContentAuthors (
 	PRIMARY KEY (content_author_id)
 );
 
-CREATE TABLE UserContentAuthors (
+CREATE TABLE "UserContentAuthors" (
 	user_content_author_id	serial,
 	user_id					integer NOT NULL,
 	content_author_id		integer NOT NULL,
 	created					timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (user_content_author_id),
-	FOREIGN KEY (user_id) REFERENCES Users,
-	FOREIGN KEY (content_author_id) REFERENCES ContentAuthors
+	FOREIGN KEY (user_id) REFERENCES "Users",
+	FOREIGN KEY (content_author_id) REFERENCES "ContentAuthors"
 );
 
-CREATE TABLE EventTypes (
+CREATE TABLE "EventTypes" (
 	event_type_id	serial,
 	name		varchar(50),
 	PRIMARY KEY (event_type_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE Events (
+CREATE TABLE "Events" (
 	event_id	serial,
 	event_type_id	integer NOT NULL,
 	predicted_release_date			timestamp,
 	predicted_textual_release_date	text,
   created		timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (event_id),
-	FOREIGN KEY (event_type_id) REFERENCES EventTypes
+	FOREIGN KEY (event_type_id) REFERENCES "EventTypes"
 );
 
-CREATE TABLE ProductMediums (
+CREATE TABLE "ProductMediums" (
 	product_medium_id	serial,
 	name				varchar(50) NOT NULL,
 	PRIMARY KEY (product_medium_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE ProductTypes (
+CREATE TABLE "ProductTypes" (
 	product_type_id	serial,
 	name			varchar(50) NOT NULL,
 	PRIMARY KEY (product_type_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE ProductStatuses (
+CREATE TABLE "ProductStatuses" (
 	product_status_id	serial,
 	name				varchar(50) NOT NULL,
 	PRIMARY KEY (product_status_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE Works (
+CREATE TABLE "Works" (
   work_id  serial,
   title             text NOT NULL,
   sort_title        varchar(50),
@@ -88,7 +88,7 @@ CREATE TABLE Works (
   PRIMARY KEY (work_id)
 );
 
-CREATE TABLE Products (
+CREATE TABLE "Products" (
 	product_id			serial,
   work_id  integer,
 	product_type_id		integer NOT NULL,
@@ -99,44 +99,44 @@ CREATE TABLE Products (
 	sort_title			varchar(50),
 	created				timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (product_id),
-  FOREIGN KEY (work_id) REFERENCES Works,
-	FOREIGN KEY (product_type_id) REFERENCES ProductTypes,
-	FOREIGN KEY (product_status_id) REFERENCES ProductStatuses,
-	FOREIGN KEY (product_medium_id) REFERENCES ProductMediums,
-  FOREIGN KEY (event_id) REFERENCES Events
+  FOREIGN KEY (work_id) REFERENCES "Works",
+	FOREIGN KEY (product_type_id) REFERENCES "ProductTypes",
+	FOREIGN KEY (product_status_id) REFERENCES "ProductStatuses",
+	FOREIGN KEY (product_medium_id) REFERENCES "ProductMediums",
+  FOREIGN KEY (event_id) REFERENCES "Events"
 );
 
-CREATE TABLE ContentAuthorProductTypes (
+CREATE TABLE "ContentAuthorProductTypes" (
 	content_author_product_type_id	serial,
 	name							varchar(50) NOT NULL,
 	PRIMARY KEY (content_author_product_type_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE ContentAuthorProducts (
+CREATE TABLE "ContentAuthorProducts" (
 	content_author_product_id		serial,
 	content_author_id				integer NOT NULL,
 	product_id						integer NOT NULL,
 	content_author_product_type_id	integer,
 	created							timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (content_author_product_id),
-	FOREIGN KEY (content_author_id) REFERENCES ContentAuthors,
-	FOREIGN KEY (product_id)	REFERENCES Products,
-	FOREIGN KEY (content_author_product_type_id) REFERENCES ContentAuthorProductTypes
+	FOREIGN KEY (content_author_id) REFERENCES "ContentAuthors",
+	FOREIGN KEY (product_id)	REFERENCES "Products",
+	FOREIGN KEY (content_author_product_type_id) REFERENCES "ContentAuthorProductTypes"
 );
 
-CREATE TABLE UserEvents (
+CREATE TABLE "UserEvents" (
 	user_event_id			serial,
 	user_id					integer NOT NULL,
 	event_id				integer NOT NULL,
 	certainty     	integer NOT NULL,
 	created					timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (user_event_id),
-	FOREIGN KEY (user_id) REFERENCES Users,
-	FOREIGN KEY (event_id) REFERENCES Events
+	FOREIGN KEY (user_id) REFERENCES "Users",
+	FOREIGN KEY (event_id) REFERENCES "Events"
 );
 
-CREATE TABLE ContentOwners (
+CREATE TABLE "ContentOwners" (
 	content_owner_id	serial,
 	name				text NOT NULL,
 	sort_name			varchar(50),
@@ -146,23 +146,23 @@ CREATE TABLE ContentOwners (
 	PRIMARY KEY (content_owner_id)
 );
 
-CREATE TABLE ContentOwnerProductTypes (
+CREATE TABLE "ContentOwnerProductTypes" (
 	content_owner_product_type_id	serial,
 	name							varchar(50),
 	PRIMARY KEY (content_owner_product_type_id),
 	UNIQUE (name)
 );
 
-CREATE TABLE ContentOwnerProducts (
+CREATE TABLE "ContentOwnerProducts" (
 	content_owner_product_id		serial,
 	content_owner_id				integer NOT NULL,
 	product_id						integer NOT NULL,
 	content_owner_product_type_id	integer,
 	created							timestamp NOT NULL DEFAULT current_timestamp,
 	PRIMARY KEY (content_owner_product_id),
-	FOREIGN KEY (content_owner_id) REFERENCES ContentOwners,
-	FOREIGN KEY (product_id) REFERENCES Products,
-	FOREIGN KEY (content_owner_product_type_id) REFERENCES ContentOwnerProductTypes
+	FOREIGN KEY (content_owner_id) REFERENCES "ContentOwners",
+	FOREIGN KEY (product_id) REFERENCES "Products",
+	FOREIGN KEY (content_owner_product_type_id) REFERENCES "ContentOwnerProductTypes"
 );
 
 COMMIT;
