18c1b56d2db0e0c54d026d4c0974d6a5ede9967e
Seb Insua <IM.Hanz@gmail.com>
2011-12-24 19:51:32 +0000

    Some kind of unit test support added but it might not work and there are not tests yet...

---
+++ modules/app/test/user.py
@@ -0,0 +1,11 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
---
+++ run_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys, os
+cwd = os.getcwd()
+
+# Activate the correct Python virtualenv.
+activate_env = cwd + "/env/bin/activate_this.py"
+execfile(activate_env, dict(__file__ = activate_env))
+
+# Insert the modules directory into the sys.path ONLY if it DOES NOT exist
+# there already.
+modules = cwd + "/modules/"
+if modules not in sys.path:
+    sys.path.append(modules)
+
+from helper.app import run_tests
+if __name__ == "__main__":
+    run_tests()
